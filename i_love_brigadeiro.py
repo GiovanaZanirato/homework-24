35da1a7a4ef0ceac247ceb1e707639a674e74123
GiovanaZanirato <84473403+GiovanaZanirato@users.noreply.github.com>
2021-05-26 16:05:48 -0300

    Add files via upload

---
+++ i_love_brigadeiro.py
@@ -0,0 +1,27 @@
+#question 2
+brigadeiro= "giovana"
+#question 3
+mom= "zanirato"
+#question 4
+dad= (brigadeiro.lower()+ " " +mom.lower())
+#question 5
+print(dad)
+#question 6
+print(dad.title())
+#question 7
+print(dad.upper())
+#question 8
+print ("giovana\nzanirato")
+#question 9
+print(100-23)
+#question 10
+print(97+12)
+#question 11
+print(14*9)
+#question 12
+print(112/18)
+#question 13
+print(1+1)
+print("1"+"1")
+print(str(1)+"1")
+print(1+int("1"))
